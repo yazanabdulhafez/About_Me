5ea20dc199e84d0da3c8a333ff450f235bf093b9
yazanabdulhafez <yazanali31@yahoo.com>
2021-06-07 18:48:08 +0300

    add js css img dir and add html files

---
+++ js/app.js
@@ -0,0 +1,61 @@
+// 'use strict';
+// //welcome part
+// let userName = prompt('please enter your name?');
+// alert('welcome to our website ' + userName + ' ,hope that you enjoy this site');
+
+// //first quistion
+// let myAge = prompt('do you think that I am over 30 years?').toLowerCase();
+// if (myAge === 'yes' || myAge === 'y') {
+//     alert('no you are wrong with that , I am 26 years old');
+// } else if (myAge === 'no' || myAge === 'n') {
+//     //console.log('you are right,I am 26 years old');
+//     alert('you are right,I am 26 years old');
+// } else {
+//     alert('please type (yes) , (y) , (no) , (n)');
+// }
+
+// //second quistion
+// let myFavDay = prompt('In your opinion do I love friday?').toLowerCase();
+// if (myFavDay === 'yes' || myFavDay === 'y') {
+//     //console.log('you hit the target , its my favotite day in the week');
+//     alert('you hit the target , its my favotite day in the week');
+// } else if (myFavDay === 'no' || myFavDay === 'n') {
+//     alert('you wrong , its my favorite day');
+// } else {
+//     alert('please type (yes) , (y) , (no) , (n)');
+// }
+
+// //third quistion
+// let techLover = prompt('do you think that I interist in technology?').toLowerCase();
+// if (techLover === 'yes' || techLover === 'y') {
+//     // console.log('you are right , technology enhance our life');
+//     alert('you are right , technology made our life easy');
+// } else if (techLover === 'no' || techLover === 'n') {
+//     alert('no you are wrong with that , I love technology   ');
+// } else {
+//     alert('please type (yes) , (y) , (no) , (n)');
+// }
+
+// //fourth quistion
+// let comeFrom = prompt('do you think that I am amarican ?').toLowerCase();
+// if (comeFrom === 'yes' || comeFrom === 'y') {
+//     alert('no you are wrong with that , I am from Jordan');
+// } else if (comeFrom === 'no' || comeFrom === 'n') {
+//     //console.log('you are right,I am from Jordan');
+//     alert('you are right,I am from Jordan ');
+// } else {
+//     alert('please type (yes) , (y) , (no) , (n)');
+// }
+
+// //fifth quistion
+// let favHoppy = prompt('I your point of veiw do I love painting?').toLowerCase();
+// if (favHoppy === 'yes' || favHoppy === 'y') {
+//     alert('yes I love painting but I preffer reading books');
+// } else if (favHoppy === 'no' || favHoppy === 'n') {
+//     // console.log('you are wrong this time , I love it  ');
+//     alert('you are wrong this time , I love it ');
+// } else {
+//     alert('please type (yes) , (y) , (no) , (n)');
+// }
+
+// alert('Its nice to have you in our website ' + userName + ' , we hope that you visit our website again');
